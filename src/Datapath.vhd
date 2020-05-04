43cf8adea4d035f1a66db350404baa2eaf7af544
Mahmoud Abd Al-Ghany <Mahmoudabdalghany@outlook.com>
2020-05-04 22:45:45 +0200

    Update Datapath.vhd

--- src/Datapath.vhd
+++ src/Datapath.vhd
@@ -20,71 +20,71 @@ end Datapath;
 architecture Structural of Datapath is 
 	component ALU is 
 		port(
-			a, b: 	   in STD_LOGIC_VECTOR(31 DOWNTO 0);
-			ALUControl: in STD_LOGIC_VECTOR(1 DOWNTO 0);
-			result:   out STD_LOGIC_VECTOR(31 DOWNTO 0);
-			ZeroFlag:    		      out STD_LOGIC
+			a, b: 	                  in STD_LOGIC_VECTOR(31 DOWNTO 0);
+			ALUControl:                in STD_LOGIC_VECTOR(1 DOWNTO 0);
+			result:                  out STD_LOGIC_VECTOR(31 DOWNTO 0);
+			ZeroFlag:    	                             out STD_LOGIC
 		);
 	end component;
 	
 	component RegFile is 
 		port(
-			clk: in STD_LOGIC;
-			WE3: in STD_LOGIC;
-			A1, A2, WA3: in STD_LOGIC_VECTOR(4 DOWNTO 0);
-			WD3: in STD_LOGIC_VECTOR(31 DOWNTO 0);
-			RD1, RD2: out STD_LOGIC_VECTOR(31 DOWNTO 0)
+			clk:                                         in STD_LOGIC;
+			WE3:                                         in STD_LOGIC;
+			A1, A2, WA3:              in STD_LOGIC_VECTOR(4 DOWNTO 0);
+			WD3:                     in STD_LOGIC_VECTOR(31 DOWNTO 0);
+			RD1, RD2:               out STD_LOGIC_VECTOR(31 DOWNTO 0)
 		);
 	end component;
 	
 	component Adder is
 		port(
-			a:  in STD_LOGIC_VECTOR(31 DOWNTO 0);
-			b:  in STD_LOGIC_VECTOR(31 DOWNTO 0);
-			y: out STD_LOGIC_VECTOR(31 DOWNTO 0)
+			a:                       in STD_LOGIC_VECTOR(31 DOWNTO 0);
+			b:                       in STD_LOGIC_VECTOR(31 DOWNTO 0);
+			y:                      out STD_LOGIC_VECTOR(31 DOWNTO 0)
 		);
 	end component;
 	
 	component SL2 is 
 		port(
-			a:  in STD_LOGIC_VECTOR(31 DOWNTO 0);
-			y: out STD_LOGIC_VECTOR(31 DOWNTO 0)
+			a:                        in STD_LOGIC_VECTOR(31 DOWNTO 0);
+			y:                       out STD_LOGIC_VECTOR(31 DOWNTO 0)
 		);
 	end component;
 	
 	component SignExtend is 
 		port(
-			a: in STD_LOGIC_VECTOR(15 DOWNTO 0);
-			y: out: STD_LOGIC_VECTOR(31 DOWNTO 0)
+			a:                       in STD_LOGIC_VECTOR(15 DOWNTO 0);
+			y:                      out STD_LOGIC_VECTOR(31 DOWNTO 0)
 		);
 	end component;
 	
 	component Reg is 
 		generic(WIDTH: integer := 32);
 		port(
-			clk, reset: in STD_LOGIC;
-			d:  in STD_LOGIC_VECTOR(WIDTH - 1 DOWNTO 0);
-			q: out STD_LOGIC_VECTOR(WIDTH - 1 DOWNTO 0)
+			clk, reset:                                  in STD_LOGIC;
+			d:                in STD_LOGIC_VECTOR(WIDTH - 1 DOWNTO 0);
+			q:               out STD_LOGIC_VECTOR(WIDTH - 1 DOWNTO 0)
 		);
 	end component;
 	
 	component Mux2 is 
 		generic(WIDTH: integer := 32);
 		port(
-			d0, d1: in STD_LOGIC_VECTOR(WIDTH - 1 DOWNTO 0);
-			s: in STD_LOGIC;
-			y: out STD_LOGIC_VECTOR(WIDTH - 1 DOWNTO 0)
+			d0, d1:           in STD_LOGIC_VECTOR(WIDTH - 1 DOWNTO 0);
+			s:                                           in STD_LOGIC;
+			y:               out STD_LOGIC_VECTOR(WIDTH - 1 DOWNTO 0)
 		);
 	end component;
 	
 	
-	SIGNAL PCplus4, PCJump, PCNext: STD_LOGIC_VECTOR(31 DOWNTO 0);
-	SIGNAL SignImm:                    STD_LOGIC_VECTOR(31 DOWNTO 0);
-	SIGNAL MulImm: 	                STD_LOGIC_VECTOR(31 DOWNTO 0);
-	SIGNAL PCBranch:                STD_LOGIC_VECTOR(31 DOWNTO 0);
-	SIGNAL WriteReg:				STD_LOGIC_VECTOR( 4 DOWNTO 0);
-	SIGNAL Result:					STD_LOGIC_VECTOR(31 DOWNTO 0);
-	SIGNAL SrcA, SrcBMux			STD_LOGIC_VECTOR(31 DOWNTO 0);
+	SIGNAL PCplus4, PCJump, PCNext:             STD_LOGIC_VECTOR(31 DOWNTO 0);
+	SIGNAL SignImm:                             STD_LOGIC_VECTOR(31 DOWNTO 0);
+	SIGNAL MulImm: 	                            STD_LOGIC_VECTOR(31 DOWNTO 0);
+	SIGNAL PCBranch:                            STD_LOGIC_VECTOR(31 DOWNTO 0);
+	SIGNAL WriteReg:              		    STD_LOGIC_VECTOR( 4 DOWNTO 0);
+	SIGNAL Result:			       	    STD_LOGIC_VECTOR(31 DOWNTO 0);
+	SIGNAL SrcA, SrcBMux			    STD_LOGIC_VECTOR(31 DOWNTO 0);
 begin
 	-- Next PC logic.
 	PCAdder: Adder(PC, X"00000004", PCplus4);
