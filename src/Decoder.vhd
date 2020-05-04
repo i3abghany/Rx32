46ba8b1109f7750dae2a6e280018c15417123e39
Mahmoud Abd Al-Ghany <Mahmoudabdalghany@outlook.com>
2020-05-05 01:04:03 +0200

    Update Decoder.vhd

--- src/Decoder.vhd
+++ src/Decoder.vhd
@@ -7,7 +7,8 @@ entity MainControlDecoder is
 		ALUOp:  out STD_LOGIC_VECTOR(1 DOWNTO 0);
 		MemWrite, MemToReg: 	   out STD_LOGIC;
 		RegDist, RegWrite:  	   out STD_LOGIC;
-		ALUSrc, branch, jump   	   out STD_LOGIC
+		ALUSrc, branch, jump   	   out STD_LOGIC;
+		jumpReg:		   out STD_LOGIC
 	);
 end MainControlDecoder;
 
@@ -16,16 +17,21 @@ architecture Behavioural of MainControlDecoder is
 begin
 	process(all) begin
 		case opcode is 
-			when "000000" => ControlSignals <= "110000010"; –– RTYPE
-			when "100011" => ControlSignals <= "101001000"; –– LW
-			when "101011" => ControlSignals <= "001010000"; –– SW
-			when "000100" => ControlSignals <= "000100001"; –– BEQ
-			when "001000" => ControlSignals <= "101000000"; –– ADDI
-			when "000010" => ControlSignals <= "000000100"; –– J
+			when "000000" => ControlSignals <= "1100000010"; –– RTYPE
+			when "100011" => ControlSignals <= "1010010000"; –– LW
+			when "101011" => ControlSignals <= "0010100000"; –– SW
+			when "000100" => ControlSignals <= "0001000001"; –– BEQ
+			when "001000" => ControlSignals <= "1010000000"; –– ADDI
+			when "000010" => ControlSignals <= "0000001000"; –– J
 			when others =>   ControlSignals <= "–––––––––"; –– illegal op
 		end case;	
 	end process;
-	(RegWrite, RegDist, ALUSrc, branch, MemWrite, MemToReg, jump, ALUOp(1 DOWNTO 0)) <= ControlSignals;
+	process(all) begin
+		if (funct = "001000" AND opcode = "000000") then 
+			ControlSignals <= "0000000100";
+		end if;
+	end process;
+	(RegWrite, RegDist, ALUSrc, branch, MemWrite, MemToReg, jump, jumpReg, ALUOp(1 DOWNTO 0)) <= ControlSignals;
 end Behavioural;
 
 -- ALU decoder.
@@ -34,7 +40,7 @@ entity ALUControlDecoder is
 	port(
 		funct: 		 in STD_LOGIC_VECTOR(31 DOWNTO 26);
 		ALUOp: 		   in STD_LOGIC_VECTOR(1 DOWNTO 0);
-		ALUControl:   out STD_LOGIC_VECTOR(2 DOWNTO 0)
+		ALUControl:       out STD_LOGIC_VECTOR(2 DOWNTO 0)
 	);
 end ALUControlDecoder;
 
@@ -51,6 +57,7 @@ begin
 					when "100100" => ALUControl <= "000" -- and.
 					when "100101" => ALUControl <= "001" -- or.
 					when "101010" => ALUControl <= "111" -- stl.
+					when "100111" => ALUControl <= "101" -- nor.
 					when others   => ALUControl <= "---" -- illegal.
 				end case;
 		end case;
