c519ac8ea01ca6a117eea7a5bcaa681688fc9d1a
Mahmoud Abd Al-Ghany <Mahmoudabdalghany@outlook.com>
2020-05-05 00:58:25 +0200

    Update mips.vhd

--- src/mips.vhd
+++ src/mips.vhd
@@ -22,36 +22,37 @@ architecture SingleCycleStructural of Mips is
 			MemWrite, MemToReg:              out STD_LOGIC; 
 			RegWrite, RegDist:               out STD_LOGIC;
 			ALUSrc, PCSrc:                   out STD_LOGIC;
-			jump:                            out STD_LOGIC
+			jump:                            out STD_LOGIC;
+			jumpReg:			 out STD_LOGIC
 		);
 	end component;
 	
 	component Datapath is 
 		port(
-			clk, reset:                    		       in STD_LOGIC;
-			MemToReg, PCSrc:               		       in STD_LOGIC;
-			ALUSrc, RegDist:               		       in STD_LOGIC;
-			RegWrite, jump:                		       in STD_LOGIC;
-			ZeroFlag:                                 out STD_LOGIC;
-			ALUControl: 			in STD_LOGIC_VECTOR(2 DOWNTO 0);
-			instr:                 in STD_LOGIC_VECTOR(31 DOWNTO 0);
-			ReadData: 			   in STD_LOGIC_VECTOR(31 DOWNTO 0);
+			clk, reset:                        	           in STD_LOGIC;
+			MemToReg, PCSrc:                   	           in STD_LOGIC;
+			ALUSrc, RegDist:                   	           in STD_LOGIC;
+			RegWrite, jump:                    	           in STD_LOGIC;
+			ZeroFlag:                                         out STD_LOGIC;
+			ALUControl: 	                in STD_LOGIC_VECTOR(2 DOWNTO 0);
+			instr:                         in STD_LOGIC_VECTOR(31 DOWNTO 0);
+			ReadData: 		       in STD_LOGIC_VECTOR(31 DOWNTO 0);
 			PC:    			   buffer STD_LOGIC_VECTOR(31 DOWNTO 0);
-			ALUOut, WriteData: buffer STD_LOGIC_VECTOR(31 DOWNTO 0)
+			ALUOut, WriteData:         buffer STD_LOGIC_VECTOR(31 DOWNTO 0)
 			);
 	end component;
 	
-	SIGNAL ZeroFlag, MemToReg, RegWrite, RegDist, ALUSrc, PCSrc, jump: STD_LOGIC;
+	SIGNAL ZeroFlag, MemToReg, RegWrite, RegDist, ALUSrc, PCSrc, jump, jumpReg: STD_LOGIC;
 	SIGNAL ALUControl: STD_LOGIC_VECTOR(2 DOWNTO 0);
 begin
 	ControlUnit: Controller port map(
 			instr(31 DOWNTO 26), instr(5 DOWNTO 0), ALUControl,
 			ZeroFlag, MemWrite, MemToReg, RegWrite, RegDist, 
-			ALUSrc, PCSrc, jump
+			ALUSrc, PCSrc, jump, jumpReg
 		);
 	MainDataPath: Datapath port map(
 		clk, reset, MemToReg, PCSrc, ALUSrc, 
-		RegDist, RegWrite, jump, ZeroFlag, 
+		RegDist, RegWrite, jump, jumpReg, ZeroFlag, 
 		ALUControl, instr, ReadData, PC,
 		ALUOut, WriteData 
 	);
