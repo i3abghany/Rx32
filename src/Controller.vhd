9c1a5da3ddf4df12dd75f4e45f46c09bbbdd95ba
Mahmoud Abd Al-Ghany <Mahmoudabdalghany@outlook.com>
2020-05-05 00:54:28 +0200

    Update Controller.vhd

--- src/Controller.vhd
+++ src/Controller.vhd
@@ -10,6 +10,7 @@ entity Controller is
 		RegWrite, RegDist:               out STD_LOGIC;
 		ALUSrc, PCSrc:                   out STD_LOGIC;
 		jump:                            out STD_LOGIC;
+		jumpReg:                         out STD_LOGIC
 	);
 end Controller;
 
@@ -21,19 +22,20 @@ architecture Structural of Controller is
 			MemWrite, MemToReg: 	   out STD_LOGIC;
 			RegDist, RegWrite:  	   out STD_LOGIC;
 			ALUSrc, branch, jump   	   out STD_LOGIC;
+			jumpReg:                   out STD_LOGIC
 		);
 	end component;
 	component ALUControlDecoder is 
 		port(
 			funct: 		 in STD_LOGIC_VECTOR(31 DOWNTO 26);
 			ALUOp: 		   in STD_LOGIC_VECTOR(1 DOWNTO 0);
-			ALUControl:   out STD_LOGIC_VECTOR(2 DOWNTO 0);
+			ALUControl:   out STD_LOGIC_VECTOR(2 DOWNTO 0)	
 		);
 	end component;
 	SIGNAL ALUOp: STD_LOGIC_VECTOR(1 DOWNTO 0);
 	SIGNAL branch: STD_LOGIC;
 begin
-	MainDecoder: MainControlDecoder port map(opcode, ALUOp, MemWrite, MemToReg, RegDist, RegWrite, ALUSrc, branch, jump);
+	MainDecoder: MainControlDecoder port map(opcode, ALUOp, MemWrite, MemToReg, RegDist, RegWrite, ALUSrc, branch, jump, jumpReg);
 	ALUDecoder: ALUControlDecoder port map(funct, ALUOp, ALUControl);
 	PCSrc <= branch AND ZeroFlag;
 end Structural;
